feb0bc2c0d77126736c0aab94dd0615854b5c90d
Generoso Redimano Jr <gredimano@objectbright.com>
2018-03-01 17:20:29 +0800

    Add reveal view finder.

---
+++ smack-app/smack-app/Controllers/ChatVC.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ChatVC: UIViewController {
+
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(
+            self.revealViewController(),
+            action: #selector(SWRevealViewController.revealToggle(_:)),
+            for: .touchUpInside
+        )
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+}
