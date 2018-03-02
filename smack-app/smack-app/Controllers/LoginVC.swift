847847784b59c7ff421dfeb06e32ba0aed32e635
Generoso Redimano Jr <gredimano@objectbright.com>
2018-03-02 08:37:38 +0800

    Setup login VC.

---
+++ smack-app/smack-app/Controllers/LoginVC.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ smack-app/smack-app/Utilities/Constants.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
