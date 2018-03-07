5cdc901afb7f3f3a1dbfa93f0c74cb5ad79c07fa
Generoso Redimano Jr <gredimano@objectbright.com>
2018-03-07 10:51:12 +0800

    Use scanner to update user image bg.

--- smack-app/smack-app/Controllers/ChannelVC.swift
+++ smack-app/smack-app/Controllers/ChannelVC.swift
@@ -5,13 +5,32 @@ class ChannelVC: UIViewController {
     // Outlets
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) { }
+    @IBOutlet weak var profileImage: CircleImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(ChannelVC.userDataDidChange(_:)),
+            name: NOTIF_USER_DATA_DID_CHANGE,
+            object: nil
+        )
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            profileImage.image = UIImage(named: UserDataService.instance.avatarName)
+            profileImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            profileImage.image = UIImage(named: "menuProfileIcon")
+            profileImage.backgroundColor = UIColor.lightGray
+        }
     }
 }
