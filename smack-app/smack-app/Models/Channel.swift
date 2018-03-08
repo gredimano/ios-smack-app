691c337c8f329ab8834311cfe03802a773b74460
Generoso Redimano Jr <gredimano@objectbright.com>
2018-03-08 10:24:09 +0800

    View all channels.

---
+++ smack-app/smack-app/Models/Channel.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Channel: Decodable {
+
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var _id: String!
+}
---
+++ smack-app/smack-app/Services/MessageService.swift
@@ -0,0 +1,40 @@
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+
+    static let instance = MessageService()
+
+    var channels = [Channel]()
+
+    func findAllChannel(completion: @escaping completionHandler) {
+        Alamofire.request(
+            URL_GET_CHANNELS,
+            method: .get,
+            parameters: nil,
+            encoding: JSONEncoding.default,
+            headers: BEARER_HEADER
+        ).responseJSON { (response) in
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                if let json = try! JSON(data: data).array {
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["_id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, _id: id)
+                        self.channels.append(channel)
+                    }
+                    
+                    completion(true)
+                }
+
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
+
