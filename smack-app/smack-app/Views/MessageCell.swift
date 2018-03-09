a7871e1ae8032ace9a47b89bc92e56a84bf77f6c
Generoso Redimano Jr <gredimano@objectbright.com>
2018-03-09 15:28:07 +0800

    Displaying chat messages.

---
+++ smack-app/smack-app/Views/MessageCell.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    // Outlets
+    @IBOutlet weak var userImage: CircleImage!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    @IBOutlet weak var messageBodyLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    func configureCell(message: Message) {
+        messageBodyLabel.text = message.message
+        userNameLabel.text = message.userName
+        userImage.image = UIImage(named: message.userAvatar)
+        userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+}
